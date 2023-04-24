de50f625f0e3a98926e22ca9de264fe17f848aeb
LISOVSMA <116366290+LISOVSMA@users.noreply.github.com>
2023-04-24 21:28:27 +0000

    Deploying to gh-pages from @ LISOVSMA/goit-js-hw-11@f62e6b3f9483cd5cd186410aa3eaf14aca10e734 🚀

---
+++ index.da31638f.js
@@ -0,0 +1,2 @@
+function changeHeaderBackground(){var e=document.querySelector(".header"),n=e.offsetHeight;window.pageYOffset>n?e.classList.add("header--no-transparency"):e.classList.remove("header--no-transparency")}window.onscroll=function(){return changeHeaderBackground()};
+//# sourceMappingURL=index.da31638f.js.map
