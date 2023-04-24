bbae1df0a3a4b040e97eccb715158d05fe955d01
LISOVSMA <116366290+LISOVSMA@users.noreply.github.com>
2023-04-25 00:27:22 +0300

    adds js

---
+++ src/js/PixabayAPI.js
@@ -0,0 +1,48 @@
+import axios from 'axios';
+
+export class PixabayAPI {
+  #BASE_URL = 'https://pixabay.com/api/';
+  #API_KEY = '35542467-e178fad6fcbc032b2bfc96ff5';
+  #query = '';
+
+  constructor() {
+    this.page = 1;
+  }
+
+  getPopularPhotos(page) {
+    return axios.get(`${this.#BASE_URL}`, {
+      params: {
+        q: 'random',
+        page,
+        per_page: 40,
+        key: this.#API_KEY,
+      },
+    });
+  }
+
+  fetchPhotosByQuery() {
+    return axios.get(`${this.#BASE_URL}`, {
+      params: {
+        q: this.#query,
+        page: this.page,
+        per_page: 40,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: ' true',
+        key: this.#API_KEY,
+      },
+    });
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.#query;
+  }
+
+  set query(newQuery) {
+    this.#query = newQuery;
+  }
+}
---
+++ src/js/gallery.js
@@ -0,0 +1,127 @@
+import { PixabayAPI } from './PixabayAPI';
+import createGalleryCards from '../templates/gallery-cards.hbs';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import Notiflix from 'notiflix';
+
+const refs = {
+  gallery: document.querySelector('.gallery'),
+  searchForm: document.querySelector('#search-form'),
+};
+
+let lightbox = new SimpleLightbox('.gallery-link', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+const pixabayApi = new PixabayAPI();
+
+async function onRenderPage() {
+  try {
+    const respons = await pixabayApi.getPopularPhotos();
+    refs.gallery.innerHTML = createGalleryCards(respons.data.hits);
+    lightbox.refresh();
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+onRenderPage();
+
+refs.searchForm.addEventListener('submit', onSearchFormSubmit);
+
+async function onSearchFormSubmit(event) {
+  event.preventDefault();
+
+  window.addEventListener('scroll', handleScroll);
+
+  refs.gallery.innerHTML = '';
+
+  const searchQuery = event.currentTarget.elements.searchQuery.value.trim();
+  pixabayApi.query = searchQuery;
+
+  pixabayApi.resetPage();
+  pixabayApi.page = 1;
+
+  if (searchQuery === '') {
+    alertNoEmptySearch();
+    return;
+  }
+
+  try {
+    const response = await pixabayApi.fetchPhotosByQuery();
+    const totalPicturs = response.data.totalHits;
+
+    if (totalPicturs === 0) {
+      alertNoEmptySearch();
+      return;
+    }
+
+    createMarkup(response.data.hits);
+    lightbox.refresh();
+    autoScroll();
+
+    Notiflix.Notify.success(`Hooray! We found ${totalPicturs} images.`);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function onLoadMore() {
+  pixabayApi.page += 1;
+
+  try {
+    const response = await pixabayApi.fetchPhotosByQuery();
+
+    const lastPage = Math.ceil(response.data.totalHits / pixabayApi.per_page);
+
+    createMarkup(response.data.hits);
+
+    lightbox.refresh();
+    autoScroll();
+
+    if (lastPage === pixabayApi.page) {
+      alertEndOfSearch();
+      window.removeEventListener('scroll', handleScroll);
+      return;
+    }
+  } catch (err) {
+    alertEndOfSearch();
+  }
+}
+
+function createMarkup(hits) {
+  const markup = createGalleryCards(hits);
+  refs.gallery.insertAdjacentHTML('beforeend', markup);
+}
+
+function alertNoEmptySearch() {
+  Notiflix.Notify.failure(
+    'Sorry, there are no images matching your search query. Please try again.'
+  );
+}
+
+function alertEndOfSearch() {
+  Notiflix.Notify.warning(
+    "We're sorry, but you've reached the end of search results."
+  );
+}
+
+function handleScroll() {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+  if (scrollTop + clientHeight >= scrollHeight - 5) {
+    onLoadMore();
+  }
+}
+
+function autoScroll() {
+  const { height: cardHeight } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
+}
